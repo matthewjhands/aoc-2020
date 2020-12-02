73047819c2938dc1a01d1ff913cf1fc2fb023520
Matthew Hands <matthewjhands@users.noreply.github.com>
2020-12-02 00:05:58 +0000

    day one part two solution

--- main/com/aoc/one/solution.java
+++ main/com/aoc/one/solution.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 class Solution {
+
     public static void main(String[] args) throws FileNotFoundException {
         final String INPUT_FILE = "main/com/aoc/one/input.txt";
 
@@ -14,22 +15,39 @@ class Solution {
         reader = new Scanner(inputFile);
         LinkedList<Integer> expenses = new LinkedList<>();
 
-        while(reader.hasNextLine()){
+        while (reader.hasNextLine()) {
             int expense = Integer.parseInt(reader.nextLine());
             expenses.add(expense);
         }
         reader.close();
 
         System.err.println("Got " + expenses.size() + " expenses from " + INPUT_FILE);
-        
+
         expenses.sort(null);
+
+        boolean foundPair = false;
+        boolean foundTriple = false;
 
         for (Integer a : expenses) {
             for (Integer b : expenses) {
-                if(a + b == 2020){
-                   System.err.println(a + " + " + b + " = 2020"); 
-                   System.err.println(a + " * " + b + " = " + (a*b));
-                   System.exit(0); // Stop execution as soon as we find a valid pair.
+                if (!foundPair && (a + b == 2020)) {
+                    System.err.println("Part One");
+                    System.err.println(a + " + " + b + " = 2020");
+                    System.err.println(a + " * " + b + " = " + (a * b));
+                    foundPair = true;
+                }
+                if (!foundTriple) {
+                    for (Integer c : expenses) {
+                        if (foundPair && foundTriple) {
+                            System.exit(0);
+                        } else if (a + b + c == 2020) {
+                            System.err.println("Part Two");
+                            System.err.println(a + " + " + b + " + " + c + " = 2020");
+                            System.err.println(a + " * " + b + " * " + c + " = " + (a * b * c));
+                            foundTriple = true;
+                            break;
+                        }
+                    }
                 }
             }
         }
