41b5f5d4e9da5e3e23d82655097a8776d315f0f0
Matthew Hands <matthewjhands@users.noreply.github.com>
2020-12-08 17:46:48 +0000

    improve template

--- main/com/aoc/template/Solution.java
+++ main/com/aoc/template/Solution.java
@@ -1,28 +1,19 @@
 package main.com.aoc.template;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
 
 class Solution {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         final String INPUT_FILE = "main/com/aoc/template/input.txt";
 
-        File inputFile = new File(INPUT_FILE);
-        Scanner reader;
-        reader = new Scanner(inputFile);
+        String input = Files.readString(Path.of(INPUT_FILE));
+        ArrayList<String> lines = new ArrayList<>(Arrays.asList(input.split("\n")));
 
-        ArrayList<char[]> map = new ArrayList<>();
-
-        while (reader.hasNextLine()) {
-            String line = reader.nextLine();
-            char[] row = line.toCharArray();
-            map.add(row);
-        }
-        reader.close();
-
-        System.err.println("Got " + map.size() + " rows from " + INPUT_FILE);
+        System.err.println("Got " + lines.size() + " rows from " + INPUT_FILE);
     }
 }
