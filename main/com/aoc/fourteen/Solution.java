9503fcec06a0770345e661a533d156fbcaaa55b1
Matthew Hands <matthewjhands@users.noreply.github.com>
2021-10-19 23:12:03 +0100

    14 - parsing lines

--- main/com/aoc/fourteen/Solution.java
+++ main/com/aoc/fourteen/Solution.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Solution {
 
@@ -15,5 +18,20 @@ class Solution {
         ArrayList<String> lines = new ArrayList<>(Arrays.asList(input.split("\n")));
 
         System.err.println("Got " + lines.size() + " rows from " + INPUT_FILE);
+
+        String currentMask;
+        HashMap<Integer,Integer> mem = new HashMap<>();
+        Pattern addressRegex = Pattern.compile("[0-9]+");
+        for(String line : lines){
+            String[] parts = line.split(" ");
+            if(parts[0].startsWith("mask")){
+                currentMask = parts[2];
+                System.err.println(currentMask);
+            } else {
+                Matcher match = addressRegex.matcher(parts[0]);
+                match.find();
+                mem.put(Integer.parseInt(match.group()), Integer.parseInt(parts[2]));
+            }
+        }
     }
 }
