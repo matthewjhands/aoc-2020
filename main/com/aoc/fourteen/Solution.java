c8519d3dbaf3788e6b7646b71e08b6b2139b8be0
Matthew Hands <matthewjhands@users.noreply.github.com>
2021-10-19 22:10:04 +0100

    day 14 ready to start

---
+++ main/com/aoc/fourteen/Solution.java
@@ -0,0 +1,19 @@
+package main.com.aoc.fourteen;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+
+    public static void main(String[] args) throws IOException {
+        final String INPUT_FILE = "main/com/aoc/fourteen/input.txt";
+
+        String input = Files.readString(Path.of(INPUT_FILE));
+        ArrayList<String> lines = new ArrayList<>(Arrays.asList(input.split("\n")));
+
+        System.err.println("Got " + lines.size() + " rows from " + INPUT_FILE);
+    }
+}
