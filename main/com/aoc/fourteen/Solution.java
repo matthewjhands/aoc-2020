b1cbc1db9ba38e2781d1cb8849968cd8962073db
Matthew Hands <matthewjhands@users.noreply.github.com>
2021-10-23 11:00:51 +0100

    day 14 part two

--- main/com/aoc/fourteen/Solution.java
+++ main/com/aoc/fourteen/Solution.java
@@ -1,6 +1,7 @@
 package main.com.aoc.fourteen;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.InvalidParameterException;
@@ -71,14 +72,62 @@ class Solution {
         return String.valueOf(result);
     }
 
-    public static void main(String[] args) throws IOException {
-        final String INPUT_FILE = "main/com/aoc/fourteen/input.txt";
+    private static String[] applyBitmaskPartTwo(String[] binaryStrings, String mask){
+        if (!mask.contains("X") && !mask.contains("1")) {
+            // no more floating points or 1's - nothing else to do.
+            return binaryStrings;
+        } 
 
-        String input = Files.readString(Path.of(INPUT_FILE));
-        ArrayList<String> lines = new ArrayList<>(Arrays.asList(input.split("\n")));
+        if (mask.contains("1")) {
+            // process all the 1's ASAP - then update the mask to avoid re-processing
+            for (int i = 0; i < binaryStrings.length; i++) {
+                // for every binary string - should just be 1 
+                char[] result = new char[MASK_WIDTH];
+                char[] maskCA = mask.toCharArray();
+                char[] stringCA = binaryStrings[i].toCharArray();
 
-        System.err.println("Got " + lines.size() + " rows from " + INPUT_FILE);
+                for (int j = 0; j < MASK_WIDTH; j++) {
+                    // for every char in string, if same char in mask is 1, replace with 1
+                    result[j] = (maskCA[j] == '1') ? '1' : stringCA[j];
+                }
+                binaryStrings[i] = String.valueOf(result);
+            }
+            // disable any further processing of 1's
+            mask = mask.replace("1", "0");
+        }
 
+        // process X's 
+
+        // identify the first X at from L to R in mask, record index
+        int xIndex = mask.indexOf("X");
+
+        ArrayList<String> newBinaryStrings = new ArrayList<>();
+        for (int i = 0; i < binaryStrings.length; i++) {
+            // for every binary string
+            
+            // copy the string, replacing the char at xIndex with 0, then save it
+            StringBuilder xAsZeroBinary = new StringBuilder(binaryStrings[i]);
+            xAsZeroBinary = xAsZeroBinary.replace(xIndex, xIndex+1, "0");
+            newBinaryStrings.add(xAsZeroBinary.toString());
+
+            // the same as above but replacing char with 1
+            StringBuilder xAsOneBinary = new StringBuilder(binaryStrings[i]);
+            xAsOneBinary = xAsOneBinary.replace(xIndex, xIndex+1, "1");
+            newBinaryStrings.add(xAsOneBinary.toString());
+        }
+        // replace this X so that we don't reprocess it
+        StringBuilder maskSB = new StringBuilder(mask);
+        maskSB = maskSB.replace(xIndex, xIndex+1, "0");
+        mask = maskSB.toString();
+
+        // then call this method again with updated mask and binaryStrings
+        String[] arr = new String[newBinaryStrings.size()];
+        arr = newBinaryStrings.toArray(arr);
+        return applyBitmaskPartTwo(arr, mask);
+
+    }
+
+    private static void partOne(ArrayList<String> lines) {
         Long sumTotal = 0L;
         String currentMask = "";
         HashMap<Integer,Long> mem = new HashMap<>();
@@ -100,6 +149,50 @@ class Solution {
         for (Integer key : mem.keySet()) {
             sumTotal += mem.get(key);
         }
-        System.err.printf("Part One | Answer is: %d", sumTotal);
+        System.err.printf("Part One | Answer is: %d\n", sumTotal);
     }
+
+    private static void partTwo(ArrayList<String> lines) {
+        Long sumTotal = 0L;
+        String currentMask = "";
+        HashMap<Long,Long> mem = new HashMap<>();
+        Pattern addressRegex = Pattern.compile("[0-9]+");
+        for(String line : lines){
+            String[] parts = line.split(" ");
+            if(parts[0].startsWith("mask")){
+                currentMask = parts[2];
+            } else {
+                Matcher match = addressRegex.matcher(parts[0]);
+                match.find();
+                String binaryRepresentation = Long.toBinaryString(Long.parseLong(match.group()));
+                binaryRepresentation = padLeftZero(binaryRepresentation, MASK_WIDTH);
+                String[] initialBinaryStrings = new String[1];
+                initialBinaryStrings[0] = binaryRepresentation;
+                String[] binaryStrings = applyBitmaskPartTwo(initialBinaryStrings, currentMask);
+
+                for(String binaryAddress : binaryStrings) {
+                    Long longAddress = Long.parseLong(binaryAddress, 2);
+                    mem.put(longAddress, Long.parseLong(parts[2]));
+                }
+            }
+        }
+        
+        for (Long key : mem.keySet()) {
+            sumTotal += mem.get(key);
+        }
+        System.err.printf("Part Two | Answer is: %d", sumTotal);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final String INPUT_FILE = "main/com/aoc/fourteen/input.txt";
+
+        String input = Files.readString(Path.of(INPUT_FILE));
+        ArrayList<String> lines = new ArrayList<>(Arrays.asList(input.split("\n")));
+
+        System.err.println("Got " + lines.size() + " rows from " + INPUT_FILE);
+
+        partOne(lines);
+        partTwo(lines);
+    }
+
 }
